7d306279fbd03137b5e50bbe1b89c30a13fe7a58
niel <niel.archer@gmail.com>
2014-05-15 23:22:03 +0100

    Trying buffered queries

--- nzedb/db/DB.php
+++ nzedb/db/DB.php
@@ -241,6 +241,7 @@ class DB extends \PDO
 		// For backwards compatibility, no need for a patch.
 		self::$pdo->setAttribute(\PDO::ATTR_CASE, \PDO::CASE_LOWER);
 		self::$pdo->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
+		self::$pdo->setAttribute(\PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true);
 	}
 
 	/**
